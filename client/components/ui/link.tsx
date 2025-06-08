ec86c2e99316987a8759fdc5304552e07d20ee24
sendo-kakeru <sendokakeru.js@gmail.com>
2025-06-08 09:20:53 +0900

    feat(client): link component

---
+++ client/components/ui/link.tsx
@@ -0,0 +1,25 @@
+import { Link as ExpoLink } from "expo-router";
+import { ComponentProps } from "react";
+import { cn } from "~/lib/utils";
+import { ChevronLeft } from "~/lib/icons/ChevronLeft";
+import { ChevronRight } from "~/lib/icons/ChevronRight";
+import { Text, View } from "react-native";
+
+const iconClassName = cn("size-2 text-sky-500");
+
+export function Link({
+  children,
+  icon,
+  className,
+  ...props
+}: ComponentProps<typeof ExpoLink> & { icon?: "right" | "left" }) {
+  return (
+    <ExpoLink className={cn("w-fit", className)} {...props}>
+      <View className="items-center flex-row gap-x-1">
+        {icon === "left" && <ChevronLeft className={iconClassName} />}
+        <Text className="text-sky-500 underline">{children}</Text>
+        {icon === "right" && <ChevronRight className={iconClassName} />}
+      </View>
+    </ExpoLink>
+  );
+}
