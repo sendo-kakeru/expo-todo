a8ceee70e54b47f16fa02194471e3891eeb62db6
sendo-kakeru <sendokakeru.js@gmail.com>
2025-06-13 23:26:45 +0900

    feat: fix

--- client/types/utils.ts
+++ client/types/utils.ts
@@ -1,8 +1,8 @@
 export type SerializeDates<T> = {
   [K in keyof T]: T[K] extends Date
     ? string
-    : T[K] extends (infer U)[] | readonly (infer U)[]
-      ? SerializeDates<U>[]
+    : T[K] extends (infer U)[] | readonly (infer V)[]
+      ? SerializeDates<U | V>[]
       : T[K] extends object
         ? SerializeDates<T[K]>
         : T[K];
