7198115739d1ff4f69bbee2e7d5bf088bfd8a30a
sendo-kakeru <sendokakeru.js@gmail.com>
2025-06-15 11:44:16 +0900

    feat: move biome

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Configuration} */
 const config = {
-  "*.{js,jsx,ts,tsx,css,json,md,yaml,yml}": ["pnpm -w prettier --write"],
+  "*.{js,jsx,ts,tsx,css,json,md,yaml,yml}": ["pnpm biome check --fix"],
 };
 
 export default config;
--- packages/shared/eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import base from "@repo/eslint-config/base";
-import rule from "@repo/eslint-config/rule";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import tsEslint from "typescript-eslint";
-
-export default tsEslint.config(base, rule, eslintConfigPrettier);
